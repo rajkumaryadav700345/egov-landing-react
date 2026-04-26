7f0e206454d39ce4a2921a45964d09825fc5cee4
rajkumaryadav700345 <rajkumaryadav700345@gmail.com>
2026-04-26 21:48:21 +0530

    Task 2 complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Features from "./Features";
+import CTA from "./CTA";
+import Latest from "./Latest";
+import Supporters from "./Supporters";
+import Footer from "./Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <Stats />
+      <Features />
+      <CTA />
+      <Latest />
+      <Supporters />
+      <Footer />
+    </>
   );
 }
 
