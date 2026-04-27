7d629a0faed09b28c7fe7ab2420eec4c41a91e7e
yadavhimanshu5051-stack <yadavhimanshu5051@gmail.com>
2026-04-27 22:04:20 +0530

    Initial commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,20 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-white shadow-sm">
+    <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
       <div className="container">
         <img src="/images/eGov-Foundation-768x291.png" alt="eGov" height="30" />
         <button
           className="navbar-toggler"
+          type="button"
           data-bs-toggle="collapse"
           data-bs-target="#nav"
+          aria-controls="nav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
